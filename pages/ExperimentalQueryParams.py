be287306c72a02848ee75638c469f9a02895c737
willhuang1997 <willhuang1997@gmail.com>
2023-12-01 11:23:12 -0800

    Update ExperimentalQueryParams.py

--- pages/ExperimentalQueryParams.py
+++ pages/ExperimentalQueryParams.py
@@ -3,6 +3,8 @@ import streamlit as st
 qp = st.experimental_get_query_params()
 st.write(qp)
 
+st.warning("Hello world")
+
 # st.experimental_set_query_params(
 #     show_map=True,
 #     selected=["asia", "america"],
